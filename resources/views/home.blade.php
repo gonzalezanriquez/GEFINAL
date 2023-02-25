029b3f25c0e31e482d00bc0097a4ba3257d01a7c
gonzalezanriquez <99001904+gonzalezanriquez@users.noreply.github.com>
2023-02-25 12:30:01 -0300

    iconos cards home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,8 +25,8 @@
         <div class="col-md-4 col-xl-3">
             <div class="card bg-c-blue order-card">
                 <div class="card-block">
-                    <h6 class="m-b-20">Orders Received</h6>
-                    <h2 class="text-right"><i class="fa fa-cart-plus f-left"></i><span>486</span></h2>
+                    <h6 class="m-b-20">Alumnos</h6>
+                    <h2 class="text-right"><i class="fa  fa-users f-left"></i><span>486</span></h2>
                     <p class="m-b-0">Completed Orders<span class="f-right">351</span></p>
                 </div>
             </div>
@@ -35,8 +35,8 @@
         <div class="col-md-4 col-xl-3">
             <div class="card bg-c-green order-card">
                 <div class="card-block">
-                    <h6 class="m-b-20">Orders Received</h6>
-                    <h2 class="text-right"><i class="fa fa-rocket f-left"></i><span>486</span></h2>
+                    <h6 class="m-b-20">Noticias</h6>
+                    <h2 class="text-right"><i class="fa fa-newspaper-o f-left"></i><span>486</span></h2>
                     <p class="m-b-0">Completed Orders<span class="f-right">351</span></p>
                 </div>
             </div>
@@ -45,22 +45,14 @@
         <div class="col-md-4 col-xl-3">
             <div class="card bg-c-yellow order-card">
                 <div class="card-block">
-                    <h6 class="m-b-20">Orders Received</h6>
-                    <h2 class="text-right"><i class="fa fa-refresh f-left"></i><span>486</span></h2>
+                    <h6 class="m-b-20">Docentes</h6>
+                    <h2 class="text-right"><i class="fa fa-coffee f-left"></i><span>486</span></h2>
                     <p class="m-b-0">Completed Orders<span class="f-right">351</span></p>
                 </div>
             </div>
         </div>
 
-        <div class="col-md-4 col-xl-3">
-            <div class="card bg-c-pink order-card">
-                <div class="card-block">
-                    <h6 class="m-b-20">Orders Received</h6>
-                    <h2 class="text-right"><i class="fa fa-credit-card f-left"></i><span>486</span></h2>
-                    <p class="m-b-0">Completed Orders<span class="f-right">351</span></p>
-                </div>
-            </div>
-        </div>
+
 	</div>
 </div>
 
