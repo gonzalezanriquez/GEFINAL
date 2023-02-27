cc4c2926fc5a92754badbf7dbc3867a2e8c458a5
gonzalezanriquez <gonzalezanriquez@gmail.com>
2023-02-27 13:34:02 -0300

    arreglos 13:33hs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,68 +3,72 @@
 @section('content')
 
 
-<div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="container fuente">
+    <div class="row justify-content-center ">
+        <div class="col-md-9">
+            <div class="card">
+                <div class="card-header fontUsuario">{{ __('Dashboard') }}
+
+                </div>
 
 
-                <div class="card-body">
+                <div class="animate__animated  animate__fadeIn pt-3 px-3">
                     @if (session('status'))
+                    <div class="nombreUsuario card-body animate__animated  animate__fadeIn">
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif
-                    <p class="">Te damos la bienvenida,
-                        <span class="fw-bold">{{ Auth::user()->name }} &#128075</span> </p>
+                        @endif
+                        <p class="fontUsuario">Te damos la bienvenida,
+                            <span class="fw-bold animate__animated  ">{{ Auth::user()->name }} &#128075</span>
+                        </p>
 
+                    </div>
 
+                    <!-- TARJETAS -->
+                    <div class="container text-center ">
+                        <div class="row d-flex justify-content-center">
+                            <div class="col-md-6 col-xl-4">
+                                <div class="card bg-c-blue order-card">
+                                    <div class="card-block">
+                                        <h6 class="m-b-20">ALUMNOS</h6>
+                                        <h2 class="text-right"><i class="bi bi-people pe-3"></i><span class="">125</span></h2>
+                                    </div>
+                                </div>
+                            </div>
 
-{{-- TARJETAS --}}
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-<div class="container">
-    <div class="row">
-        <div class="col-md-4 col-xl-3">
-            <div class="card bg-c-blue order-card">
-                <div class="card-block">
-                    <h6 class="m-b-20">Alumnos</h6>
-                    {{-- <h2 class="text-right"><i class="fa  fa-users f-left counter"></i><span>{{$activePosts}}</span></h2> --}}
-                    <p class="m-b-0 counter">Completed Orders<span class="f-right counter">351</span></p>
+                            <div class="col-md-6 col-xl-4">
+                                <div class="card bg-c-green order-card">
+                                    <div class="card-block">
+                                        <h5 class="">NOTICIAS</h5>
+                                        <h2 class="text-right"><i class="bi bi-chat-right-text pe-3"></i><span>325</span></h2>
+                                        <!-- {{-- <h2 class="text-right"><i class="fa fa-newspaper-o f-left"></i><span>{{$activePosts}}</span></h2> --}} -->
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6 col-xl-4">
+                                <div class="card bg-c-yellow order-card">
+                                    <div class="card-block">
+                                        <h6 class="m-b-20">Docentes</h6>
+                                        <h2 class="text-right"><i class="bi bi-people pe-3"></i><span>325</span></h2>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
-
-        <div class="col-md-4 col-xl-3">
-            <div class="card bg-c-green order-card">
-                <div class="card-block">
-                    <h6 class="m-b-20">Noticias</h6>
-                    {{-- <h2 class="text-right"><i class="fa fa-newspaper-o f-left"></i><span>{{$activePosts}}</span></h2> --}}
-                    <p class="m-b-0">Completed Orders<span class="f-right">351</span></p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-4 col-xl-3">
-            <div class="card bg-c-yellow order-card">
-                <div class="card-block">
-                    <h6 class="m-b-20">Docentes</h6>
-                    <h2 class="text-right"><i class="fa fa-coffee f-left"></i><span>486</span></h2>
-                    <p class="m-b-0">Completed Orders<span class="f-right">351</span></p>
-                </div>
-            </div>
-        </div>
-
-
-	</div>
-</div>
-
-
-
-
+        <div class="col-md-1">
+            <div class="card">
+                <div class="card-header fontUsuario">{{ __('Dashboard') }}
 
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,55 +1,62 @@
 @extends('layouts.sidebar')
 
 @section('content')
-<div class="container" >
-    <div class="px-4 text-center d-flex justify-content-center align-content-center " >
+<div class="container">
+    <div class="px-4 text-center d-flex justify-content-center align-content-center ">
 
         <div class="col-md-12">
-        {{-- <img class="mx-auto " src="{{asset('/img/news.png')}}"  alt="Image Noticias" height="200px">--}}
+            {{-- <img class="mx-auto" src="{{asset('/img/news.png')}}" alt="Image Noticias" height="200px">--}}
 
-                <div class="card">
-                    <div class="card-body">
-                        <div class="d-flex justify-content-center my-4">
-                            <h1>Lista de Usuarios</h1>
-                        </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-center my-4">
+                        <h1>Lista de Usuarios</h1>
+                    </div>
 
-                        <a class="btn btn-dark btn-sm mb-4" href="{{route('users.create')}}">
-                            Crear Nuevo Usuario
-                        </a>
+                    <a class="btn btn-dark btn-sm mb-4" href="{{route('users.create')}}">
+                        Crear Nuevo Usuario
+                    </a>
 
-                        <!-- TABLA -->
-                        <table id="table" class="table">
-                            <thead>
+                    <!-- TABLA -->
+                    <table id="table" class="table">
+                        <thead>
                             <tr>
                                 <th class=" d-none d-xl-table-cell" scope="col ">Id</th>
-                                <th  scope="col">Nombre</th>
+                                <th scope="col">Nombre</th>
                                 <th class=" d-none d-xl-table-cell" scope="col">Email</th>
+                                <th class=" d-none d-xl-table-cell" scope="col">Rol</th>
                                 <th style="text-align: end" scope="col">Acciones</th>
                             </tr>
-                            </thead>
-                            <tbody>
+                        </thead>
+                        <tbody>
                             @foreach($users as $user)
-                                <tr>
-                                    <th class=" d-none d-xl-table-cell" scope="row">{{$user->id}}</th>
-                                    <td>{{$user->name}}</td>
-                                    <td class=" d-none d-xl-table-cell">{{$user->email}}</td>
-                                    <td style="text-align: end">
+                            <tr>
+                                <th class=" d-none d-xl-table-cell" scope="row">{{$user->id}}</th>
+                                <td>{{$user->name}}</td>
+                                <td class=" d-none d-xl-table-cell">{{$user->email}}</td>
+                                <td class=" d-none d-xl-table-cell">{{$user->roleId}}</td>
+                                <td style="text-align: end">
+                                    <!-- EDITAR -->
+                                    <a class="btn btn-success btn-sm mb-1" href="{{route('users.edit', ['id'=>$user->id])}}">
+                                        <i class=" bi bi-pencil "></i></a>
+                                    <!-- ASIGNAR ROLES -->
+                                    <a class="btn btn-warning btn-sm mb-1" href="{{route('roles.index', ['id'=>$user->id])}}">
+                                        <i class=" bi bi-person-vcard "></i></a>
+                                    <!-- DELETE -->
+                                    <a class="btn btn-danger btn-sm mb-1" href="{{route('users.delete', ['id'=> $user->id])}}">
+                                        <i class=" bi bi-trash "></i></a>
 
-                                        <a class="btn btn-danger btn-sm mb-1" href="{{route('users.delete', ['id'=> $user->id])}}">Eliminar</a>
-                                        <a class="btn btn-success btn-sm mb-1" href="{{route('users.edit', ['id'=>$user->id])}}">Editar</a>
-                                        <a class="btn btn-warning btn-sm mb-1" href="{{route('roles.index', ['id'=>$user->id])}}">Roles</a>
-
-                                    </td>
-                                </tr>
+                                </td>
+                            </tr>
                             @endforeach
-                            </tbody>
-                        </table>
-                        {!! $users->links() !!}
-                    </div>
+                        </tbody>
+                    </table>
+                    {!! $users->links() !!}
                 </div>
+            </div>
+        </div>
     </div>
-  </div>
-  </div>
+</div>
 
 
 <!-- 
