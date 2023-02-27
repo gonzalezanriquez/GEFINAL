eb806ec0753a6406f34c8aa5aee427a4c930014e
Khercog <kevin.hercog@davinci.edu.ar>
2023-02-27 00:11:38 -0300

    antes de pisarlo

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -34,11 +34,7 @@ class AuthServiceProvider extends ServiceProvider
             return $user->username === 'AdminUser';
         });
 
-        Gate::define('create_user', [User::class, 'store']);
-        Gate::define('create_post', [Post::class, 'store']);
-        Gate::define('create_subject', [Subject::class, 'store']);
-        Gate::define('create_level', [LevelsController::class, 'store']);
-        Gate::define('create_subject', [Post::class, 'store']);
+
 
 
         // create_user, create_post, create_subject, create_level, update_user, update_post, update_subject, update_classroom, destroy_user, destroy_post, destroy_role, destroy_subject, destroy_classroom, show_user, show_post, show_postUser, show_subject, show_classroom
