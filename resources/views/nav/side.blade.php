e4fada42a116b6082a23e8363c9050580aad50c4
gonzalezanriquez <99001904+gonzalezanriquez@users.noreply.github.com>
2023-02-28 21:03:03 -0300

    28-02-23 21:03

--- resources/views/nav/side.blade.php
+++ resources/views/nav/side.blade.php
@@ -15,11 +15,17 @@
 
                 <li class="{{ Request::is('posts*') ? 'active' : '' }}">
                     <a href="{{ url('/posts') }}" class="nav-link " aria-current="page">
-                        <i class="bi bi-envelope pe-2"></i> Posteos Internos</a>
+                        <i class="bi bi-envelope pe-2"></i> Noticias Internass</a>
                 </li>
                 <li class="{{ Request::is('posts*') ? 'active' : '' }}">
                     <a href="{{ url('/posts') }}" class="nav-link " aria-current="page">
-                        <i class="bi bi-envelope pe-2"></i> Noticias</a>
+                        <i class="bi bi-envelope pe-2"></i> Noticias Externas</a>
+                <li class="{{ Request::is('posts*') ? 'active' : '' }}">
+                    <a href="{{ url('/posts') }}" class="nav-link " aria-current="page">
+                        <i class="bi bi-envelope pe-2"></i> Posteos Internos</a>
+                <li class="{{ Request::is('posts*') ? 'active' : '' }}">
+                    <a href="{{ url('/posts') }}" class="nav-link " aria-current="page">
+                        <i class="bi bi-envelope pe-2"></i> Posteos Externos</a>
                 </li>
                 <li class="{{ Request::is('users*') ? 'active' : '' }}">
                     <a href="{{ url('/users') }}" class="nav-link " aria-current="page">
