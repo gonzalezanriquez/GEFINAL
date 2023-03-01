016a87e4ba75f2093fecaa97a85c49665d760685
gonzalezanriquez <99001904+gonzalezanriquez@users.noreply.github.com>
2023-03-01 19:28:51 -0300

    DASHBOARD

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,131 @@
 @extends('layouts.sidebar')
 
 @section('content')
-<div class="container fuente">
+
     <div class="row justify-content-center ">
         <div class="col-md-9">
-            <div class="card">
+
+    <div class="main-body">
+    
+          <div class="row gutters-sm">
+            <div class="col-md-4 mb-3">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex flex-column align-items-center text-center">
+                    <img src="https://cdn.kastatic.org/images/avatars/svg/piceratops-seedling.svg" alt="Admin" class="rounded-circle" width="150">
+                    <div class="mt-3">
+                      <h2>{{ Auth::user()->name }}  </h2>
+                      <p class="text-secondary mb-1">Te damos la bienvenida.Aqui podras encontrar las funcionalidades disponibles</p>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="card mt-3">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe mr-2 icon-inline"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>USUARIOS </h6>
+                    <span class="text-secondary">https://bootdey.com</span>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github mr-2 icon-inline"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>Github</h6>
+                    <span class="text-secondary">bootdey</span>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter mr-2 icon-inline text-info"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>Twitter</h6>
+                    <span class="text-secondary">@bootdey</span>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram mr-2 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>Instagram</h6>
+                    <span class="text-secondary">bootdey</span>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook mr-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
+                    <span class="text-secondary">bootdey</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-8">
+              <div class="card mb-3">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Nombre</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        
+                    <span class="badge bg-info"> {{ Auth::user()->name }}  </span>
+                   
+                                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0"> Usuario</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <span class="badge bg-warning"> {{ Auth::user()->username }}  </span>
+
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Email</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <span class="badge bg-danger">  {{ Auth::user()->email }}   </span>
+
+                   
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Rol</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <span class="badge bg-secondary">  {{ Auth::user()->roles->implode('name',' - ') }}   </span></div>
+                  </div>
+                
+                
+                </div>
+              </div>
+
+              <div class="row gutters-sm justify-content-center">
+              
+                <div class="d-flex justify-content-center">
+                    <button class="btn btn-warning">GESTIONAR USUARIOS</button>
+                    <button>BOTON</button>
+                </div>              
+
+                
+              </div>
+
+
+
+          </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 <div class="card-header fontUsuario">{{ __('Dashboard') }}</div>
                 <div class="animate__animated  animate__fadeIn pt-3 px-3">
                     @if (session('status'))
@@ -20,45 +141,10 @@
                     </div>
 
                     <!-- TARJETAS -->
-                    <div class="container text-center ">
-                        <div class="row d-flex justify-content-center">
-                            <div class="col-md-6 col-xl-4">
-                                <div class="card bg-c-blue order-card">
-                                    <div class="card-block">
-                                        <h5 class="m-b-20">ALUMNOS</h5>
-                                        <h2 class="text-right"><i class="bi bi-people pe-3"></i><span
-                                                class="">125</span></h2>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-md-6 col-xl-4">
-                                <div class="card bg-c-green order-card">
-                                    <div class="card-block">
-                                        <h5 class="">NOTICIAS</h5>
-                                        <h2 class="text-right"><i
-                                                class="bi bi-chat-right-text pe-3"></i><span>{{$activePosts}}</span>
-                                        </h2>
-                                        <!-- {{-- <h2 class="text-right"><i class="fa fa-newspaper-o f-left"></i><span>{{$activePosts}}</span></h2> --}} -->
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-md-6 col-xl-4">
-                                <div class="card bg-c-yellow order-card">
-                                    <div class="card-block">
-                                        <div class="datetime">
-                                            <div class="h2 time "></div>
-                                            <div class="h5 date"></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    
 
                 </div>
-            </div>
+    
 
         </div>
     </div>
